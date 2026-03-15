c481662874594399e3a9e65b5575eee03c35a50e
Matheus <mattexorc1@gmail.com>
2026-03-15 19:05:33 -0300

    15/03/2026 19:05:33,36 auto update

---
+++ backend/demo/src/main/java/com/example/demo/dto/UsuarioRequest.java
@@ -0,0 +1,52 @@
+package com.example.demo.dto;
+
+public class UsuarioRequest {
+    private String nome;
+    private String username;
+    private String senha;
+    private String role;
+    private Boolean ativo;
+
+    public UsuarioRequest() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
